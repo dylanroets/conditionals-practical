e43d7eaadd2e6315792d59f91dcb93a78e5dd666
Dylan Roets <dylanroets@yahoo.com>
2022-10-02 11:58:26 -0500

    added first conditionals, working fine, couple of formatting issues

--- conditionals.js
+++ conditionals.js
@@ -1,1 +1,10 @@
-console.log( 'js' );
+let seatInCar = 4;
+let passengers = 5;
+
+if ( seatInCar >= passengers ) {
+    console.log( 'lets ride' );
+    
+} 
+else {
+    console.log( 'we are not ready. check car, passengers, equipment' );
+}
