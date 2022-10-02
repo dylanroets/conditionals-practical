bddc11197a34a362a931e18b311d0b3bc744c52f
Dylan Roets <dylanroets@yahoo.com>
2022-10-02 12:07:55 -0500

    added boolean and working fine

--- conditionals.js
+++ conditionals.js
@@ -2,9 +2,15 @@ let seatInCar = 7;
 let passengers = 5;
 let infants = 1
 let infantSeats = 1
+let fullTank = true;
 if ( seatInCar >= passengers && infantSeats >= infants ) {
     console.log( 'lets ride' );
-    
+    if ( fullTank ) {
+        console.log( 'all gassed up and ready' );
+    }
+    else{
+        console.log( 'we need to fill up' );
+    }
 } 
 else {
     console.log( 'we are not ready. check car, passengers, equipment' );
